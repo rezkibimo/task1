21848465a9ee115c66264fbc581ba8d7f0e0f936
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-09 19:26:59 +1100

    onclick fetch json test 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('http://universities.hipolabs.com/search?country=Australia', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+
   return (
-    <div className="App">
-    <br></br>
-    <div className='container'>
-      <button type="button" class="btn btn-lg btn-light">GET</button> &nbsp;
-      <button type="button" class="btn btn-lg btn-light">POST</button> &nbsp;
-      <button type="button" class="btn btn-lg btn-light">PUT</button>&nbsp;
-      <button type="button" class="btn btn-lg btn-light">DELETE</button> &nbsp;
-      <hr />
-      </div>
+    <div>
+      {err && <h2>{err}</h2>}
+
+      <button onClick={handleClick}>Fetch data</button>
+
+      {isLoading && <h2>Loading...</h2>}
+
+      {data.data.map(university => {
+        return (
+          <div key={university.name}>
+            <h2>{university.name}</h2>
+            <h2>{university.name}</h2>
+            <h2>{university.name}</h2>
+            <br />
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
