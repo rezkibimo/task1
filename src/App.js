cd61d77e91f7b348aabe51a672007a078331b8ff
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-09 18:21:26 +1100

    basic react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <br></br>
+    <div className='container'>
+      <button type="button" class="btn btn-lg btn-light">GET</button> &nbsp;
+      <button type="button" class="btn btn-lg btn-light">POST</button> &nbsp;
+      <button type="button" class="btn btn-lg btn-light">PUT</button>&nbsp;
+      <button type="button" class="btn btn-lg btn-light">DELETE</button> &nbsp;
+      <hr />
+      </div>
     </div>
   );
 }
