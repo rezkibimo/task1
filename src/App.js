d847de13db00635180f02f1cef4b503045fda289
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-09 20:31:12 +1100

    new JSON url implemented
    JSON onclick now works with the required API
    
    extra: Bootstrap styling for NAV, Loading indicator and Table

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  const [data, setData] = useState({ data: [] });
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
 
@@ -17,7 +17,7 @@ const App = () => {
         },
       });
 
-      console.log('data is: ', JSON.stringify(data, null, 4));
+      console.log(JSON.stringify(data, null, 4));
 
       setData(data);
     } catch (err) {
@@ -37,23 +37,25 @@ const App = () => {
       <button onClick={handleClick} className='btn btn-light btn-lg'>Fetch data</button>
       <button className='btn btn-light btn-lg'>Clear data</button>
 
-      {isLoading && <h2>Loading...</h2>}
+      {isLoading && <button className='btn btn-primary' type="button" disabled><span className='spinner-grow spinner-grow-sm' role="status" aria-hidden="true"></span>Loading</button>}
 
       <table className='table table-sm'>
         <thead>
           <tr>
             <th scope="col">Name</th>
-            <th scope="col">Name</th>
-            <th scope="col">Name</th>
+            <th scope="col">Country Code</th>
+            <th scope="col">Country</th>
+            <th scope="col">Webpage</th>
           </tr>
         </thead>
         <tbody>
-            {data.data.map(university => {
+            {data.map(university => {
               return (
                 <tr key={university.name}>
                   <td>{university.name}</td>
-                  <td>{university.name}</td>
-                  <td>{university.name}</td>
+                  <td>{university.alpha_two_code}</td>
+                  <td>{university.country}</td>
+                  <td>{university.web_page}</td>
                 </tr>
               );
             })}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <nav className='navbar bg-light'>
+      <div className='container-fluid'>
+        <span className='navbar-brand mb-0 h1'>University List</span>
+        <br></br>
+      </div>
+    </nav>
     <App />
   </React.StrictMode>
 );
